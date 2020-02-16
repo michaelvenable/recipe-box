73df6d68441760ff02cd35247a97988057d3248c
Michael Venable <mvenable@gmx.com>
2020-02-16 01:52:35 -0600

    Extract edit recipe to component

--- client/src/pages/edit.js
+++ client/src/pages/edit.js
@@ -1,42 +1,29 @@
 import React from 'react';
 import Axios from 'axios';
 
+import EditRecipeForm from '../components/edit-recipe-form';
+
 export default class EditRecipePage extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            name: '',
-            ingredients: [],
-            steps: []
+            recipe: null
         };
     }
 
     componentDidMount() {
         Axios.get(`https://a2j4q04p5g.execute-api.us-east-2.amazonaws.com/prod/recipes/${this.props.match.params.name}`)
-        .then(response => this.setState(response.data))
+        .then(response => this.setState({ recipe: response.data }))
         .catch(error => console.error(error));
     }
-
-    handleIngredientsChange(e) {
-        this.setState({
-            ingredients: e.target.value.split('\n')
-        });
-    }
-
-    handleStepsChange(e) {
-        this.setState({
-            steps: e.target.value.split('\n')
-        });
-    }
-
     handleSubmit(e) {
-        Axios.put(
-            `https://a2j4q04p5g.execute-api.us-east-2.amazonaws.com/prod/recipes/${this.state.name}`,
-            this.state
-        )
-        .then(response => this.props.history.push('/recipes'))
-        .catch(error => console.error(error));
+        // Axios.put(
+        //     `https://a2j4q04p5g.execute-api.us-east-2.amazonaws.com/prod/recipes/${this.state.name}`,
+        //     this.state
+        // )
+        // .then(response => this.props.history.push('/recipes'))
+        // .catch(error => console.error(error));
 
         e.preventDefault();
     }
@@ -48,7 +35,11 @@ export default class EditRecipePage extends React.Component {
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-12">
-                                <h2>Edit {this.state.name}</h2>
+                                {
+                                    this.state.recipe == null
+                                        ? <p>Loading</p>
+                                        : <h2>Edit {this.state.recipe.name}</h2>
+                                }
                             </div>
                         </div>
                     </div>
@@ -57,39 +48,11 @@ export default class EditRecipePage extends React.Component {
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-12">
-                                <form onSubmit={this.handleSubmit.bind(this)}>
-                                    <div className="form-group">
-                                        <label htmlFor="ingredients">Ingredients:</label>
-                                        <textarea id="ingredients"
-                                                  className="form-control"
-                                                  value={this.state.ingredients.join('\n')}
-                                                  onChange={this.handleIngredientsChange.bind(this)}
-                                                  rows="4"
-                                                  required="required"></textarea>
-                                    </div>
-
-                                    <div className="form-group">
-                                        <label htmlFor="directions">Directions:</label>
-                                        <textarea id="directions"
-                                                  value={this.state.steps.join('\n')}
-                                                  onChange={this.handleStepsChange.bind(this)}
-                                                  className="form-control"
-                                                  rows="4"
-                                                  required="required"></textarea>
-                                    </div>
-
-                                    <div className="container">
-                                        <div className="row">
-                                            <div className="col-lg-4 col-md-2">
-                                            </div>
-                                            <div className="col-lg-4 col-md-8">
-                                                <button type="submit" className="btn btn-primary btn-submit">Save Changes</button>
-                                            </div>
-                                            <div className="col-lg-4 col-md-2">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
+                                {
+                                    this.state.recipe === null
+                                        ? <p>Loading</p>
+                                        : <EditRecipeForm recipe={this.state.recipe} />
+                                }
 
                                 <form style={{ marginTop: "30px" }}>
                                     <div className="container">
